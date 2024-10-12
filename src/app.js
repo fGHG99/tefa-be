a42d6c2d1aa3e4e73e654c4ad6a5867193e23cf9
fGHG99 <azkalearn10@gmail.com>
2024-10-12 21:35:39 +0700

    askndsakdn

---
+++ src/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const Authcontroller = require('./user/controllers/AuthController');
+const UserController = require('./user/controllers/UserController');
+const ScannerRoute = require('./user/routes/ScannerRoute');
+const FavoriteRoute = require('./user/routes/FavoriteRoute');
+const CartRoute = require('./user/routes/CartrRoute');
+const OrderRoute = require('./user/routes/OrderRoute');
+const HistoryRoute = require('./user/routes/HistoryRoute');
+const ProductRoute = require('./user/routes/ProductRoute');
+const ConfigRoute = require('./user/routes/FeeRoute');
+
+const app = express();
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://mesan.curaweda.com",
+        "https://api-mesan.curaweda.com",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "Origin",
+        "Accept",
+    ],
+    optionsSuccessStatus: 200,
+};
+
+// Apply CORS middleware
+app.options('*', cors(corsOptions)); // Allow preflight requests for all routes
+
+
+// Parse JSON requests
+app.use(express.json());
+
+
+//? CORS SECTION END
+
+// Routes
+app.use('/auth', Authcontroller);
+app.use('/scanner', ScannerRoute);
+app.use('/orders', OrderRoute);
+app.use('/favorites', FavoriteRoute);
+app.use('/cart', CartRoute);
+app.use('/history', HistoryRoute);
+app.use('/product', ProductRoute);
+app.use('/config', ConfigRoute);
+app.use('/user', UserController);
+
+// Server
+const PORT = process.env.PORT || "3001";
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
