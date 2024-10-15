847503c9d1fdc6635e9942e03e2064cc2b8138b5
Lietreum <127808721+Lietreum@users.noreply.github.com>
2024-10-15 22:10:31 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 
 // Catch-all for 404 Not Found (if no routes matched)
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'Route not found' });
+    res.status(404).json({ error: 'trigger happy pack heat but its blank ink cheetoh' });
 });
 
 // Server
