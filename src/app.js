124e929cde153200c84019829bdc412abb4baa3e
fGHG99 <azkalearn10@gmail.com>
2024-10-15 19:28:30 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -14,9 +14,7 @@ const app = express();
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "http://localhost:3001",
         "https://mesan.curaweda.com",
-        "https://api-mesan.curaweda.com",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
