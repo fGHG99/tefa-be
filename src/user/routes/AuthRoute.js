a505f6882eae6af73f5a0103a1cf72533144025e
Lietreum <127808721+Lietreum@users.noreply.github.com>
2024-10-16 13:12:06 +0700

    Update AuthRoute.js

--- src/user/routes/AuthRoute.js
+++ src/user/routes/AuthRoute.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { register, login, logout } = require('../controllers/AuthController');
-const { protect } = require('../middlewares/Middleware'); 
+const { register, login, logout, refreshToken } = require('../controllers/AuthController');
+const { protect, authorizeRoles } = require('../middlewares/Middleware'); 
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', protect, logout); 
+router.post('/logout', protect, logout);
+router.post('/refreshtoken', refreshToken);  // New refresh token endpoint
+
 router.get('/', protect, (req, res) => {
-    return res.status(200).json({ message: 'Authenticated', user: req.user });
+  return res.status(200).json({ message: 'Authenticated', user: req.user });
 });
 
 module.exports = router;
