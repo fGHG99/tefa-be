145ad061802805276881e8b209ed4658af1d3500
fGHG99 <azkalearn10@gmail.com>
2024-10-16 08:40:53 +0700

    changing message

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 
 // Catch-all for 404 Not Found (if no routes matched)
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'trigger happy pack heat but its blank ink cheetoh' });
+    res.status(404).json({ error: 'Cannot find the route' });
 });
 
 // Server
