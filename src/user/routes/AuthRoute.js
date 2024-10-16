5e28a164aebfd92f2ce9ede7779c8a396b353096
Lietreum <127808721+Lietreum@users.noreply.github.com>
2024-10-16 13:28:23 +0700

    Update AuthRoute.js

--- src/user/routes/AuthRoute.js
+++ src/user/routes/AuthRoute.js
@@ -9,6 +9,10 @@ router.post('/login', login);
 router.post('/logout', protect, logout);
 router.post('/refreshtoken', refreshToken);  // New refresh token endpoint
 
+router.get('/test', (req, res) => {
+  res.status(200).json({ message: 'Test route is working!' });
+});
+
 router.get('/', protect, (req, res) => {
   return res.status(200).json({ message: 'Authenticated', user: req.user });
 });
