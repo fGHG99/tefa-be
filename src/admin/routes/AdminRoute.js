ccd435c8cbf33538e07859cad3815408a0d4130b
Rakan007 <rakandanendrad@gmail.com>
2024-10-22 08:05:38 +0700

    p

--- src/admin/routes/AdminRoute.js
+++ src/admin/routes/AdminRoute.js
@@ -55,8 +55,8 @@ const feeController = require('../controllers/FeeController'); // Updated import
   });
 
 // Routes for managing users
-router.post('/create', protect, adminController.createUser);
-router.get('/users', protect, adminController.getAllUsers);
+router.post('/create', adminController.createUser);
+router.get('/users',  adminController.getAllUsers);
 router.put('/update/:id', protect, authorizeRoles('ADMIN'), adminController.updateUser);
 router.delete('/delete/:id', protect, authorizeRoles('ADMIN'), adminController.deleteUser);
 
